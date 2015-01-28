3b6864545d8a45959f3b4a5fcf60f47032330cf5
Fengda HUANG <h@phodal.com>
2015-01-28 23:29:03 +0800

    Server | simple server example

---
+++ server.py
@@ -0,0 +1,24 @@
+import falcon
+
+
+class ThingsResource:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def on_get(req, resp):
+        """Handles GET requests"""
+        resp.status = falcon.HTTP_200  # This is the default status
+        resp.body = ('\nTwo things awe me most, the starry sky '
+                     'above me and the moral law within me.\n'
+                     '\n'
+                     '    ~ Immanuel Kant\n\n')
+
+# falcon.API instances are callable WSGI apps
+app = falcon.API()
+
+# Resources are represented by long-lived class instances
+things = ThingsResource()
+
+# things will handle all requests to the '/things' URL path
+app.add_route('/things', things)
