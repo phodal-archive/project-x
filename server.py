78bc1dfed18434d812ea3d6e17cb1a7b9e52e0a7
Fengda HUANG <h@phodal.com>
2015-01-28 23:36:28 +0800

    Server | move blog to server

--- server.py
+++ server.py
@@ -1,24 +1,18 @@
 import falcon
+from blog_models import *
 
 
-class ThingsResource:
+class PostsResource:
     def __init__(self):
         pass
 
     @staticmethod
     def on_get(req, resp):
-        """Handles GET requests"""
-        resp.status = falcon.HTTP_200  # This is the default status
-        resp.body = ('\nTwo things awe me most, the starry sky '
-                     'above me and the moral law within me.\n'
-                     '\n'
-                     '    ~ Immanuel Kant\n\n')
+        posts = WpPosts.get()
+        resp.status = falcon.HTTP_200
+        resp.body = posts.post_content
 
-# falcon.API instances are callable WSGI apps
+
 app = falcon.API()
 
-# Resources are represented by long-lived class instances
-things = ThingsResource()
-
-# things will handle all requests to the '/things' URL path
-app.add_route('/things', things)
+app.add_route('/things', PostsResource())
