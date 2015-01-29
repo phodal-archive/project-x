43be26af6bb4394c7ca95ba6b7cd57b14d4fa74a
Fengda HUANG <h@phodal.com>
2015-01-29 11:12:23 +0800

    Add Flask & sample all posts

--- server.py
+++ server.py
@@ -16,7 +16,22 @@ class PostsResource():
                                 "author": user.display_name})
 
 
+class AllPostsResource():
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def on_get(req, resp, post_id):
+        post = WpPosts.get(WpPosts.id == post_id)
+        user = WpUsers.get(WpUsers.id == post.post_author)
+        resp.status = falcon.HTTP_200
+        resp.body = json.dumps({"post_content": post.post_content,
+                                "author": user.display_name})
+
+
 app = application = falcon.API()
 
 posts = PostsResource()
+allPosts = AllPostsResource()
 app.add_route('/posts/{post_id}', posts)
+app.add_route('/posts/all', allPosts)
