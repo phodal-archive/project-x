f16535accc726d22693c72930361e22ff35ccb88
Fengda HUANG <h@phodal.com>
2015-01-29 13:56:13 +0800

    continue all posts work

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import json
 import falcon
 from blog_models import *
 
+app = application = falcon.API()
 
 class PostsResource():
     def __init__(self):
@@ -21,17 +22,18 @@ class AllPostsResource():
         pass
 
     @staticmethod
-    def on_get(req, resp, post_id):
-        post = WpPosts.get(WpPosts.id == post_id)
-        user = WpUsers.get(WpUsers.id == post.post_author)
+    def on_get(req, resp):
+        result = dict()
+        blog_posts = WpPosts.select().where(WpPosts.post_status == "publish")
+        for post in blog_posts:
+            result.update({"posts": post.post_content})
+
         resp.status = falcon.HTTP_200
-        resp.body = json.dumps({"post_content": post.post_content,
-                                "author": user.display_name})
+        resp.body = 'hello'
 
-
-app = application = falcon.API()
 
 posts = PostsResource()
 allPosts = AllPostsResource()
+
+app.add_route('/all/posts', allPosts)
 app.add_route('/posts/{post_id}', posts)
-app.add_route('/posts/all', allPosts)
