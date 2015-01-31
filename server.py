0add16eed2469c4912204c58dcd08b4d84f1ec9a
Fengda HUANG <h@phodal.com>
2015-01-31 20:38:31 +0800

    diffrent part for server

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import falcon
 from flask import Flask, request, send_from_directory
+from gevent import monkey
 from gevent.pywsgi import WSGIServer
 
 from api.post import PostsResource, AllPostsResource, CommentsResource, PostsCommentsResource
@@ -12,19 +13,18 @@ app = Flask(__name__, static_folder='static', static_url_path='')
 def sitemap():
     return send_from_directory(app.static_folder, request.path[1:])
 
-    return response
-
-
 api = falcon.API()
 
 api.add_route('/posts/{post_id}', PostsResource())
 api.add_route('/posts/{post_id}/comments', PostsCommentsResource())
 api.add_route('/comment/{comment_id}', CommentsResource())
 api.add_route('/all/posts', AllPostsResource())
-#
-# if __name__ == '__main__':
-#     server = WSGIServer(('0.0.0.0', 8000), api)
-#     server.serve_forever()
 
-if __name__ == "__main__":
-    app.run()
+
+def main():
+    monkey.patch_thread()
+
+server = WSGIServer(('', 8000), api)
+server.serve_forever()
+http_server = WSGIServer(('', 5000), app)
+http_server.serve_forever()
