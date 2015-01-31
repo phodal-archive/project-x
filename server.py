a9adffbfaeed15cdb456b4db1548b7f40332929e
Fengda HUANG <h@phodal.com>
2015-01-31 20:21:00 +0800

    small api on server

--- server.py
+++ server.py
@@ -3,18 +3,12 @@ from gevent.wsgi import WSGIServer
 
 from api.post import PostsResource, AllPostsResource, CommentsResource, PostsCommentsResource
 
+api = falcon.API()
 
-api = application = falcon.API()
-
-posts = PostsResource()
-allPosts = AllPostsResource()
-commentsResource = CommentsResource()
-postsComments = PostsCommentsResource()
-
-api.add_route('/posts/{post_id}', posts)
-api.add_route('/posts/{post_id}/comments', postsComments)
-api.add_route('/comment/{comment_id}', commentsResource)
-api.add_route('/all/posts', allPosts)
+api.add_route('/posts/{post_id}', PostsResource())
+api.add_route('/posts/{post_id}/comments', PostsCommentsResource())
+api.add_route('/comment/{comment_id}', CommentsResource())
+api.add_route('/all/posts', AllPostsResource())
 
 if __name__ == '__main__':
     server = WSGIServer(('0.0.0.0', 8000), api)
