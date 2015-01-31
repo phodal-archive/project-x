f7d25d139dac4e91b6f264389eace6ac965cdaba
Fengda HUANG <h@phodal.com>
2015-01-31 20:33:28 +0800

    Test for sitemap

--- server.py
+++ server.py
@@ -1,7 +1,19 @@
 import falcon
-from gevent.wsgi import WSGIServer
+from flask import Flask, request, send_from_directory
+from gevent.pywsgi import WSGIServer
 
 from api.post import PostsResource, AllPostsResource, CommentsResource, PostsCommentsResource
+
+
+app = Flask(__name__, static_folder='static', static_url_path='')
+
+
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory(app.static_folder, request.path[1:])
+
+    return response
+
 
 api = falcon.API()
 
@@ -9,7 +21,10 @@ api.add_route('/posts/{post_id}', PostsResource())
 api.add_route('/posts/{post_id}/comments', PostsCommentsResource())
 api.add_route('/comment/{comment_id}', CommentsResource())
 api.add_route('/all/posts', AllPostsResource())
+#
+# if __name__ == '__main__':
+#     server = WSGIServer(('0.0.0.0', 8000), api)
+#     server.serve_forever()
 
-if __name__ == '__main__':
-    server = WSGIServer(('0.0.0.0', 8000), api)
-    server.serve_forever()
+if __name__ == "__main__":
+    app.run()
