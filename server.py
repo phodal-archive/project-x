4899bb7016fb6aec944492c8b8e12cfaba11dc0a
Fengda HUANG <h@phodal.com>
2015-02-01 13:21:29 +0800

    use wsgi replace pywsgi, wsgi performance better than pywsgi

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import falcon
 from flask import Flask, render_template
 from gevent import monkey
-from gevent.pywsgi import WSGIServer
+from gevent.wsgi import WSGIServer
 
 from api.post import PostsResource, AllPostsResource, CommentsResource, PostsCommentsResource
 
@@ -21,7 +21,6 @@ app.register_blueprint(frontends_module)
 from sitemap.views import mod as sitemap_module
 app.register_blueprint(sitemap_module)
 
-
 api = falcon.API()
 
 api.add_route('/posts/{post_id}', PostsResource())
@@ -29,13 +28,13 @@ api.add_route('/posts/{post_id}/comments', PostsCommentsResource())
 api.add_route('/comment/{comment_id}', CommentsResource())
 api.add_route('/all/posts', AllPostsResource())
 
-#
-# def main():
-#     monkey.patch_thread()
+def main():
+    monkey.patch_thread()
+
 #
 # server = WSGIServer(('0.0.0.0', 8000), api)
 # server.serve_forever()
-# http_server = WSGIServer(('0.0.0.0', 5000), app)
-# http_server.serve_forever()
+http_server = WSGIServer(('0.0.0.0', 5000), app)
+http_server.serve_forever()
 
-app.run()
+# app.run()
