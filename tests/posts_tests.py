2898ff25ded66dda275e7a7e21b40e792f45c476
Fengda HUANG <h@phodal.com>
2015-02-08 20:31:34 +0800

    add selenium test

---
+++ tests/posts_tests.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_search_in_python_org(self):
+        url = "http://127.0.0.1:5000/create/articles"
+        driver = self.driver
+        driver.get(url)
+        self.assertIn("http://127.0.0.1:5000/login", driver.current_url)
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
