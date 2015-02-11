7fc5d4806ef46546b4a72d7f2379f2323eaa736e
Fengda HUANG <h@phodal.com>
2015-02-11 22:47:17 +0800

    use lazy_text replace gettext

--- xunta/users/forms.py
+++ xunta/users/forms.py
@@ -3,22 +3,19 @@
 from flask_babel import gettext
 
 from flask_wtf import Form
-from wtforms import PasswordField, BooleanField, StringField
-from wtforms.fields.html5 import URLField
+from wtforms import PasswordField, StringField
 from wtforms.validators import EqualTo, Email, DataRequired
+from flask_babel import lazy_gettext as _
 
 
 class RegisterForm(Form):
-    name = StringField(gettext('Nick Name'), validators=[DataRequired()])
-    email = StringField(gettext('Email'), validators=[DataRequired(), Email()])
-    password = PasswordField(gettext('Password'), validators=[DataRequired()])
-    confirm = PasswordField(gettext('Confirm'), validators=[
+    name = StringField(_('Nick Name'), validators=[DataRequired()])
+    email = StringField(_('Email'), validators=[DataRequired(), Email()])
+    password = PasswordField(_('Password'), validators=[DataRequired()])
+    confirm = PasswordField(_('Confirm'), validators=[
         DataRequired(),
-        EqualTo('password', message=gettext('Passwords must match'))
+        EqualTo('password', message=_('Passwords must match'))
     ])
-    # accept_tos = BooleanField(gettext('I accept the Terms of Service.'), validators=[DataRequired()])
-    # recaptcha = RecaptchaField()
-
 
 class LoginForm(Form):
     email = StringField(gettext('Email'), validators=[DataRequired(), Email()])
