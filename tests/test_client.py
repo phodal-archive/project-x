93c423984703120af22e7be072addf4fb890568b
Fengda HUANG <h@phodal.com>
2015-02-12 09:49:07 +0800

    add prepare article

--- tests/test_client.py
+++ tests/test_client.py
@@ -3,6 +3,15 @@ from flask import url_for
 from mongoengine import connect
 from suite import BaseSuite
 from xunta import create_app
+from xunta.articles.Articles import Article, Tag
+from xunta.users.User import User
+
+
+def prepare_account(self):
+    with self.app.test_request_context():
+        user = User(name='foo', email='foo@email.com', password='1')
+        user.save()
+        return user
 
 
 class TestSignup(BaseSuite):
@@ -13,6 +22,7 @@ class TestSignup(BaseSuite):
         self.app_context = self.app.app_context()
         self.app_context.push()
         self.client = self.app.test_client(use_cookies=False)
+        self.user = prepare_account(self)
 
     def tearDown(self):
         self.app_context.pop()
@@ -35,6 +45,22 @@ class TestSignup(BaseSuite):
         response = self.client.post(self.url_for('users.login'), data={
             'email': 'john@example.com',
             'password': 'hello'
-        })
-        print response.data
-        assert "Redirecting..." in response.data
+        }, follow_redirects=True)
+        self.client.post(self.url_for('users.logout'))
+        assert "wrapper" in response.data
+
+
+    def prepare_article(self):
+        print self.user.email
+        self.client.get(self.url_for("articles.create_articles"))
+        tag = Tag(name='tag', description='tag description')
+        tag.save()
+        article = Article(title="title",
+                          description="description",
+                          content="this is content",
+                          slug='hello',
+                          author=self.user.get_mongo_doc(),
+                          tag=tag.get_mongo_doc())
+        article.save()
+        for article in article.get_all_articles():
+            assert 'description' in article.description
